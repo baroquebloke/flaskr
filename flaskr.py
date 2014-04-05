cda7f71b4e4abd4bbd3b838b64f350cd655f251e
baroquebloke <flutephoto@gmail.com>
2014-04-05 15:06:34 -0400

    further into docs tutorial.  Not sure if I should have split some of this code into seperate files...

--- flaskr.py
+++ flaskr.py
@@ -10,7 +10,7 @@ app.config.from_object(__name__)
 
 #load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db')
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     DEBUG=True,
     SECRET_KEY='development key',
     USERNAME='admin',
@@ -23,6 +23,28 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def get_db():
+    """
+    Opens a new database connection if there is none yet for the current
+    application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+def close_db(error):
+    """closes database again at the end of the request"""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 
 if __name__ == '__main__':
     app.run()
