0c9b89c0fa03ca30c622b87cee44bf235712a17a
Jacob Farnsworth <Baroquebloke@Jacobs-MacBook.local>
2014-05-07 19:14:39 -0400

    custom 404.html displays, but not updating with edited content.  Why?

--- flaskr.py
+++ flaskr.py
@@ -20,9 +20,9 @@ def show_page(path):
     except TemplateNotFound:
         return render_template('404.html'), 404
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
